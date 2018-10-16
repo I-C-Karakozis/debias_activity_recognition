a5b8584847a402822e3c8df0368644fbaf53adae
Yannis Karakozis <ick@wic-medialab-davis.Princeton.EDU>
2018-10-16 13:44:12 -0400

    merge install.sh and setup.sh

--- setup.sh
+++ setup.sh
@@ -11,11 +11,25 @@ source venv/bin/activate
 # install dependencies
 pip install -r requirements.txt
 
+# setup directories
+mkdir model_output
+
 # download data
-./install.sh
+curl https://s3.amazonaws.com/my89-frame-annotation/public/of500_images_resized.tar > resized.tar
+tar -xvf resized.tar
+mv of500_images_resized resized_256
+
+mkdir baseline_models
+curl https://s3.amazonaws.com/my89-frame-annotation/public/baseline_encoder > baseline_models/baseline_encoder
+curl https://s3.amazonaws.com/my89-frame-annotation/public/baseline_resnet_101 > baseline_models/baseline_resnet_101
+curl https://s3.amazonaws.com/my89-frame-annotation/public/baseline_resnet_50 > baseline_models/baseline_resnet_50
+curl https://s3.amazonaws.com/my89-frame-annotation/public/baseline_resnet_34 > baseline_models/baseline_resnet_34
+
 rm resized.tar
 
-#setup directories
-mkdir model_output
+# preprocess data
+python preprocess_train.py data/train.json data/genders_train.json data/human_verbs.txt
+python preprocess_test.py data/human_verbs.txt data/test.json data/genders_test.json 
+python preprocess_test.py data/human_verbs.txt data/dev.json data/genders_dev.json
 
 # {u'frames': [{u'item': u'n07678729', u'destination': u'n03619890', u'place': u'n01794158', u'agent': u'n10787470'}], u'verb': u'stuffing'}
