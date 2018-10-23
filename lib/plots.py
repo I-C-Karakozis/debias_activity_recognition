c930fe1f5e2a701dacbf23fcf27c3fe65bbfc1b4
Yannis Karakozis <ick@visualai-2.cs.princeton.edu>
2018-10-22 20:46:49 -0400

    plotting with shifted figures

--- lib/plots.py
+++ lib/plots.py
@@ -10,7 +10,8 @@ def plot_accuracy_per_activity(accuracies_per_activity, encoder):
     activities_ordered = [encoder.decode_verb(index) for index in indices_ordered]
     activity_acc_sorted = numpy.sort(accuracies_per_activity).tolist()
     min_index = len(activity_acc_sorted) - activity_acc_sorted[::-1].index(-1)
-    print(activities_ordered)
-    plt.bar(indices_ordered[min_index:], activity_acc_sorted[min_index:])
+    shifted_indices = [i for i in range(min_index, len(activity_acc_sorted))]
+    plt.bar(shifted_indices, activity_acc_sorted[min_index:])
+    plt.xticks(shifted_indices, activities_ordered)
     #plt.plot([i for i in range(len(accuracies_per_activity))], accuracies_per_activity, '-o')
     plt.savefig("figures/acc_per_activity")
